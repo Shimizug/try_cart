e42b82b451f72e2d623ec644236a5f21db2a249c
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-07-17 04:29:43 +0000

    [Add]routeの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'care_items/index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root :to =>"homes#top"
 
-  resources :cart_item, only: [:create, :destroy, :update, :index] do
-    collection do
-      delete :destroy_all
-    end
-  end
-
+  resources :cart_item, only: [:create, :destroy, :update, :index] 
+  delete 'cart_items/destroy_all' => 'cart_items#destroy_all', as: 'destroy_all'
 
 end
